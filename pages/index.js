8573636474323e39cb5f89c470feb329f83456d7
TwistZA <avi3110@gmail.com>
2021-01-21 23:47:38 +0200

    Revert minor change

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>404 Page Not Found!!</title>
+        <title>404 Page Not Found</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
